c1940dc3656dc598933468074870fa14dc81971b
Scott Butler <scottbutler@northwesternmutual.com>
2020-11-11 13:03:30 -0600

    boot screen, player selection, and spirtes added. Collision with nugget too

--- src/game/scenes/BootScene.js
+++ src/game/scenes/BootScene.js
@@ -1,20 +1,84 @@
 import { Scene } from 'phaser'
 
 import labBG from '@/game/assets/CCLabsBackground.png'
+import titleBG from '@/game/assets/CCL.jpg'
 import tommy from '@/game/assets/Tommy.png'
-
+import david from '@/game/assets/David.png'
+import manske from '@/game/assets/Manske.png'
+import omar from '@/game/assets/Omar.png'
+import bravo from '@/game/assets/Bravo.png'
+import jordan from '@/game/assets/Jordan.png'
 
 export default class BootScene extends Scene {
   constructor () {
     super({ key: 'BootScene' })
+    this.heroName = '';
+    this.players = [];
   }
 
   preload () {
     this.load.image('labBG', labBG)
-    this.load.image('hero', tommy)
+    this.load.image('titleBG', titleBG)
+    this.load.image('tommy', tommy)
+    this.load.image('david', david)
+    this.load.image('manske', manske)
+    this.load.image('omar', omar)
+    this.load.image('nugget', bravo)
+    this.load.image('enemy', jordan)
   }
 
   create () {
-    this.scene.start('PlayScene')
+    console.log("boot this", this)
+    // Background
+    this.add.image(303, 299, 'titleBG')
+    this.add.rectangle(303, 299, 400, 400, 0xffffff, .9)
+
+    // Instruction text
+    this.instructions = this.add.text(150, 50, 'Select your player, then press ENTER', {fontSize: '16px', fill: '#FFF'})
+
+    // player choice sprites
+    this.tommy = this.add.sprite(200, 200, 'tommy').setInteractive()
+    this.players.push(this.tommy);
+    this.tommy.alpha = 0.6
+    this.tommy.on('pointerdown', () => {
+      this.selectSprite(this.tommy)
+    })
+
+    this.david = this.add.sprite(400, 200, 'david').setInteractive()
+    this.players.push(this.david);
+    this.david.alpha = 0.6
+    this.david.on('pointerdown', () => {
+      this.selectSprite(this.david)
+    })
+
+    this.manske = this.add.sprite(200, 400, 'manske').setInteractive()
+    this.players.push(this.manske);
+    this.manske.alpha = 0.6
+    this.manske.on('pointerdown', () => {
+      this.selectSprite(this.manske)
+    })
+
+    this.omar = this.add.sprite(400, 400, 'omar').setInteractive()
+    this.players.push(this.omar);
+    this.omar.alpha = 0.6
+    this.omar.on('pointerdown', () => {
+      this.selectSprite(this.omar)
+    })
+
+    this.enterKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER)
+  }
+  
+  selectSprite(sprite) {
+    this.heroName = sprite.texture.key
+    this.players.forEach( player => player.alpha = .6)
+    sprite.alpha = 1.0
+  }
+  
+  update() {
+    if (this.enterKey.isDown)
+    {
+      this.scene.start('PlayScene')
+    }
   }
 }
+
--- src/game/scenes/PlayScene.js
+++ src/game/scenes/PlayScene.js
@@ -1,21 +1,70 @@
 import { Scene } from 'phaser'
 
-
 export default class PlayScene extends Scene {
   constructor () {
     super({ key: 'PlayScene' })
+    this.score = 0;
+    this.heroSpeed = 2;
+    this.collisionDist = 35;
+    this.enemySpeed = 2;
   }
 
   create () {
-    this.add.image(300, 300, 'labBG')
+    console.log("play this", this)
+    console.log("this.game.scene.scenes[0].heroName", this.game.scene.scenes[0].heroName)
+    // background
+    this.add.image(303, 299, 'labBG')
 
-    // const bomb = this.physics.add.image(400, 200, 'bomb')
-    // bomb.setCollideWorldBounds(true)
-    // bomb.body.onWorldBounds = true // enable worldbounds collision event
-    // bomb.setBounce(1)
-    // bomb.setVelocity(200, 20)
+    // hero
+    this.hero = this.add.sprite(100, 150, this.game.scene.scenes[0].heroName)
+    this.hero.setScale(.4)
+    
+    // nugget
+    this.nugget = this.add.sprite(Phaser.Math.RND.between(20, 580), Phaser.Math.RND.between(20, 580), 'nugget')
+    this.nugget.setScale(.4)
+
+    // enemy
+    this.enemy = this.add.sprite(500, 500, 'enemy')
+    this.enemy.setScale(.4)
+
+    // score
+    this.scoreText = this.add.text(10, 10, 'score: '+this.score, {fontSize: '32px', fill: '#FFF'})
+    
+    // bind keys
+    this.leftKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT)
+    this.rightKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT)
+    this.downKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN)
+    this.upKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP)
   }
 
   update () {
+    if (this.leftKey.isDown)
+    {
+      this.hero.x -= this.heroSpeed;
+    }
+    if (this.rightKey.isDown)
+    {
+      this.hero.x += this.heroSpeed;
+    }
+    if (this.upKey.isDown)
+    {
+      this.hero.y -= this.heroSpeed;
+    }
+    if (this.downKey.isDown)
+    {
+      this.hero.y += this.heroSpeed;
+    }
+
+    if(this.hero.x - this.nugget.x <= this.collisionDist 
+      && this.hero.x - this.nugget.x >= -this.collisionDist
+      && this.hero.y - this.nugget.y <= this.collisionDist
+      && this.hero.y - this.nugget.y >= -this.collisionDist)
+    { 
+      this.nugget.x = Phaser.Math.RND.between(20, 580)
+      this.nugget.y = Phaser.Math.RND.between(20, 580)
+      this.score += 10
+      this.scoreText.setText("score: " + this.score)
+    }
   }
+
 }
